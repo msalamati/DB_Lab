a8125eff7d04df7e7124de8198f81fa20875a183
Mohammad Salamati <salamati9819@gmail.com>
2021-04-23 17:55:08 +0430

    Add jobseekers module, service & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { JobseekersModule } from './jobseekers/jobseekers.module';
 import UserEntity from './db/entity/user.entity';
 import BooksModule from './books/books.module';
 import GenreModule from './genre/genre.module';
@@ -22,6 +23,8 @@ import GenreEntity from './db/entity/genre.entity';
     TypeOrmModule.forRoot(),
 
     AuthModule,
+
+    JobseekersModule,
 
   ],
   controllers: [AppController],
